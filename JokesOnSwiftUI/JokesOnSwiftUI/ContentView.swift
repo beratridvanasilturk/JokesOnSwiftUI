a8cd4392dfb44e3536446c301103e51c263855aa
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-14 01:50:02 +0300

    Added Some Comments

--- JokesOnSwiftUI/JokesOnSwiftUI/ContentView.swift
+++ JokesOnSwiftUI/JokesOnSwiftUI/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
             }
         }
     }
-        // API duzgun calisirsa listeye yeni joke ekler
+        // API duzgun calisirsa listeye yeni joke ekler.
         func addJoke() {
             jokesVM.getJokes()
     }
